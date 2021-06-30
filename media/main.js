0ccc7e401765b04072ed50adca1feae8d95800f7
Ralph Marzetti <rmzetti@gmail.com>
2021-06-30 15:07:18 +1200

    first commit

---
+++ media/main.js
@@ -0,0 +1,8 @@
+//import * as vscode from "vscode";
+const vscode=acquireVsCodeApi();
+function mem1 () {
+  const button=document.getElementById('m1');
+  button.innerText='main.js';
+  //console.log("hello!"); //to the developer tools window
+}
+mem1();
